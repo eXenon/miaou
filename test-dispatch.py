69568c011d0120e11a387b0a0c5ed842680ac17a
Auditor <auditor@pentest-PwC.(none)>
2015-03-24 22:31:38 +0100

    Deleted old files.

--- test-dispatch.py
+++
@@ -1,5 +0,0 @@
-#/usr/bin/python3
-
-import dispatch
-
-dispatch.dispatch("perl /tools/network/unix/massping/massping-h.pl -s {{file:/home/auditor/reports/Rexel/04-Pays-Bas/massping/ips.txt}} -o /home/auditor/reports/Rexel/04-Pays-Bas/top1000 -p top1000")
--- test-walkftp.py
+++
@@ -1,6 +0,0 @@
-
-#/usr/bin/python3
-
-import walkftp
-
-walkftp.ftp_walk("10.182.0.148")
