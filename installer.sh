241f1182d5020ee79889c45303c68106af6da3b4
Auditor <auditor@pentest-PwC.(none)>
2015-03-27 10:50:06 +0100

    Added correct directory resolution.

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-currentdir=$(pwd)
+currentdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ending='$PYTHONPATH'
 echo "export PYTHONPATH=$currentdir:$ending" >> ~/.bashrc
 source ~/.bashrc
